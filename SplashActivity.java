55de2f1f0fc0c40817d25914e780bbb6ab8b9f9b
samuelnoahosa <94325714+samuelnoahosa@users.noreply.github.com>
2022-02-25 23:12:49 -0500

    Add files via upload

---
+++ SplashActivity.java
@@ -0,0 +1,28 @@
+package com.example.myfirstsecondapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        Button btn = (Button)findViewById(R.id.btnStart);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(SplashActivity.this, MainActivity.class));
+                finish();
+
+            }
+        });
+    }
+}
